1272735e1384fa579e4b79507c55f3f7155626d9
RachKozy <rkozy1@kent.edu>
2016-04-21 17:30:22 -0400

    Refined Home page and Plans page. Need to continue work on about and blog page
    Also added in PHP for footer and header

---
+++ dev/prototype/templates/about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!-- Include Header.php -->
+  <?php include("partials/header.html") ?>
+
+
+      <main class="aboutpage row">
+
+         <div class="column row">
+           <div class="row column">
+             <h1>About Reading Buzz</h1>
+             </div>
+
+             <p class="call-to-action small-10 small-offset-1 medium-8 medium-offset-2 columns">Learning is easy with Reading Buzz’s digital content library of educational videos and blog posts. Our content is delivered to schools weekly by MLIS Children’s Librarian.</p>
+
+              <p class="small-10 small-offset-1 medium-8 medium-offset-2 columns">
+                Facing budget cuts, decreased staff and an evolving educational landscape, school libraries across the country need a solution that is accessible and affordable to supplement their existing services. Reading Buzz addresses this need with a comprehensive educational service offering that includes:
+                <em>weekly lesson plans for school librarians and students, notification of new, trending and must-read books, story time and read-along sessions.</em>
+              </p>
+
+              <div class="plans-signup small-6 small-offset-3 medium-4 medium-offset-4 columns">
+                <a href="signup.html">Learn More</a>
+              </div>
+          </div>
+
+          <hr />
+
+
+          <div class="biography">
+            <div class="small-8 small-offset-2 medium-3 medium-offset-0 columns">
+              <img class="lindsey-img" src="assets/lindsey.jpg"></img>
+            </div>
+
+            <div class="small-8 small-offset-2 medium-8 medium-offset-0 columns">
+              <h4 class="meetlindsey">Meeting Lindsey Buzzelli</h4>
+
+              <p class="bio-p">Lindsey is an accredited Children’s and Young Adult Librarian/School Librarian via Kent State University. Her passion for working with students of all ages and her love of reading inspired her to become a Children’s/Young Adult Librarian. In her library classes, students are encouraged to be hands on: sing, dance, have fun and become inspired by reading and Information Literacy to grow into lifelong learners.
+              </p>
+
+              <ul>
+                <li>Favorite Children’s Picture Book: Tap the Magic Tree</li>
+                <li>Favorite Children’s Chapter Book: The Secret Garden</li>
+                <li>Favorite Young Adult Book: The Giver</li>
+              </ul>
+            </div>
+
+          </div>
+
+
+
+      </main>
+
+
+        <!-- Include Footer.html -->
+        <?php include("partials/footer.html") ?>
---
+++ dev/prototype/templates/blog.php
@@ -0,0 +1,36 @@
+<!-- Include Header.php -->
+    <?php include("partials/header.html") ?>
+
+      <div class="row">
+        <div class="small-10 medium-8 small-centered columns">Blog Image<br><br><br><br><br><br><br><br><br><br></div>
+        <div class="small-10 medium-8 small-centered columns">Blog Post Title Description<br><br><br><br><br><br></div>
+      </div>
+
+      <div class="row">
+        <div class="column row">
+           <div class="small-8 small-offset-2 medium-2 medium-offset-3 columns">
+             <h3>Blog Title</h3>
+             <ul>
+               <li>Short description about blog post</li>
+             </ul>
+           </div>
+           <div class="small-8 small-offset-2 medium-2 medium-offset-0 columns">
+             <h3>Blog Title</h3>
+             <ul>
+               <li>Short description about blog post</li>
+             </ul>
+           </div>
+           <div class="small-8 small-offset-2 medium-2 medium-offset-0 columns">
+             <h3>Blog Title</h3>
+             <ul>
+               <li>Short description about blog post</li>
+             </ul>
+           </div>
+       </div>
+      </div>
+
+
+
+
+<!-- Include Footer.html -->
+<?php include("partials/footer.html") ?>
