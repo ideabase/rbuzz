d6457fe460e1e9e9c608c17f77775605a7fd7e95
Christopher Hallahan <challahan@gmail.com>
2016-04-28 00:08:10 -0400

    Light CSS updates to templates, created landing page
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/plans.php
+++ dev/prototype/templates/plans.php
@@ -10,7 +10,7 @@
 
            <div class="row column">
              <h1>Plans</h1>
-             <p>Knowledge is power, and at Reading Buzz, we put the power in your hands by offering access to a library of educational videos and blog posts. We offer three core plans ranging from basic to fully customizable, with face-to-face options available. Bolster the educational services offered through your library by signing up with Reading Buzz today.</p>
+             <p>Knowledge is power, and at Reading Buzz, we make school library lessons easy by offering access to a library of educational videos and tips for busy elementary school librarians. We offer three core plans ranging from basic to fully customizable, with face-to-face options available.</p>
              </div>
 
              <div class="plan1 small-10 small-offset-1 medium-3 medium-offset-0 columns">
@@ -18,9 +18,9 @@
                <hr />
                <h6 class="access1">Includes:</h6>
                <ul>
-                 <li class="planli-1">Weekly 20 minute lessons for PreK-8</li>
-                 <li class="planli-2">Monthly tip video</li>
-                 <li class="planli-3">Full access to ReadingBuzz.us</li>
+                 <li class="planli-1">Weekly 20 minute lessons for Pre-K through 8th grade</li>
+                 <li class="planli-2">Monthly tip videos</li>
+                 <li class="planli-3">Full access to lesson archives</li>
                  <li class="price">$250</li>
                  <li class="permonth">per month</li>
                  <li class="peryear">or $2,400 per school year</li>
@@ -32,9 +32,9 @@
                <hr />
                <h6 class="access2">Includes:</h6>
                <ul>
-                 <li class="planli-1">Monthly live sessions for PreK-8</li>
+                 <li class="planli-1">Monthly live sessions for Pre-K through 8th grade</li>
                  <li class="planli-2">Monthly tip videos</li>
-                 <li class="planli-3">Full access to ReadingBuzz.us</li>
+                 <li class="planli-3">Full access to lesson archives</li>
                  <li class="price">$300</li>
                  <li class="permonth">per month</li>
                  <li class="peryear">or $3,600 per school year</li>
@@ -46,10 +46,10 @@
                <hr />
                <h6 class="access3">Includes:</h6>
                <ul>
-                 <li class="planli-1">Customizable videos</li>
+                 <li class="planli-1">Customizable videos for Pre-K through 8th grade</li>
                  <li class="planli-2">Monthly live video sessions</li>
                  <li class="planli-3">Monthly tip videos</li>
-                 <li class="planli-4">Full access to ReadingBuzz.us</li>
+                 <li class="planli-4">Full access to lesson archives</li>
                  <li class="price">$400</li>
                  <li class="permonth">per month</li>
                  <li class="peryear">or $4,800 per school year</li>
@@ -58,12 +58,15 @@
 
         </div>
 
+        <span>Schools may request an additional 3 videos for summer months.  Yearly contracts are available upon request.</span>
+
         <div class="row column" clearfix>
             <div class="whichplan">
-              <h4>Prefer Teaching In-Person?</h4>
-                <p>Librarians and librarian aides who support face-to-face services will receive monthly tip videos on how to grow their collection, manage circulation, and understanding fundraising, donations and volunteers.</p>
+              <h4>What Do Lessons Include?</h4>
+                <p>Our weekly lesson videos include standard lessons, readalongs, book talks and story times.  Librarians and librarian aides who support face-to-face services will also receive monthly tip videos on how to grow their collection, manage circulation, and understanding fundraising, donations and volunteers.</p>
 
               <div class="plan-videos">
+                <h4>View Sample Videos</h4>
                 <div class="plan-video small-10 small-offset-1 medium-4 medium-offset-0 columns">
                   Video
                 </div>
